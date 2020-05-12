0c09677475c723577fb0069f085516ba0cc56ec8
jimihunter2002 <jimi.hunter008@gmail.com>
2020-05-12 23:39:45 +0100

    login endpoint pointer corrected

--- helsinki-fullstack/part5/bloglist-frontend/src/services/login.js
+++ helsinki-fullstack/part5/bloglist-frontend/src/services/login.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseUrl = 'http://localhost:3001/api/login';
-const baseUrl = '/api/persons';
+const baseUrl = '/api/login';
 
 const login = async credentials => {
   const response = await axios.post(baseUrl, credentials);
